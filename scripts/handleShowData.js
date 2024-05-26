bd90fd6b9da516258d32c8f5aa80e39f8212ec35
Aamreet <amreetsingh508@gmail.com>
2024-05-26 15:59:26 +0530

    updated ui and added remove record functionality

--- scripts/handleShowData.js
+++ scripts/handleShowData.js
@@ -1,40 +1,47 @@
+import { handleDelete } from "./handleDelete.js";
+
 export const handleShowData = (db) => {
-    let showButton = document.querySelector('#show');
+    let showButton = document.querySelector("#show");
     let recordContainer = document.querySelector("#r-list");
-    console.log(showButton);
     let htmlContent = "";
     if (showButton.innerText === "Show") {
-        const transaction = db.transaction(['data'], 'readonly');
-        const objectStore = transaction.objectStore('data');
+        const transaction = db.transaction(["data"], "readonly");
+        const objectStore = transaction.objectStore("data");
         const cursorRequest = objectStore.openCursor();
         cursorRequest.onsuccess = function (event) {
             const cursor = event.target.result;
             if (cursor) {
-                // Display data in the UI
-                console.log('Title:', cursor.value.title, 'Value:', cursor.value.value);
-
                 htmlContent += `
                  <div class="sub-container">
+                 <div class="title-container">
                  <h2 class="title">${cursor.value.title}</h1>
+                 <button id="${cursor.value.title}" class="remove">
+                 X
+             </button>
+                 </div>
                  <p class="val">${cursor.value.value}</p>
                </div>
                  `;
+                
                 cursor.continue();
-            } else {
-                console.log('No more data');
-                console.log(htmlContent);
+            } 
+            else {
                 recordContainer.innerHTML = htmlContent;
-                showButton.innerText = "Close"
+                showButton.innerText = "Close";
                 recordContainer.classList.remove("display-none");
+                document.querySelectorAll('.remove').forEach(item => {
+                     item.addEventListener("click",()=>{
+                        handleDelete(db,item.id);
+                     });
+                });
             }
         };
         cursorRequest.onerror = function (event) {
-            console.error('Error reading data from IndexedDB');
+            console.error("Error reading data from IndexedDB");
         };
-    }
-    else if (showButton.innerText === "Close") {
-        showButton.innerText = "Show"
+    } else if (showButton.innerText === "Close") {
+        showButton.innerText = "Show";
         recordContainer.innerHTML = "";
         recordContainer.classList.add("display-none");
     }
-}
+};
